8353d1e4c96c4fd3af5f234dd5a550a94d0be490
Guillaume Hamel <guillaume.hamel@efrei.net>
2021-03-01 00:23:24 +0100

    Support search actions

--- app/src/main/java/com/test/gittracker/MainActivity.java
+++ app/src/main/java/com/test/gittracker/MainActivity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.ViewCompat;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
 
@@ -26,7 +33,8 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
-    MyAdapter myAdapter = new MyAdapter();
+    private MyAdapter myAdapter = new MyAdapter();
+    private String target = "Naegon";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,21 +46,66 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setNestedScrollingEnabled(listView, true);
 
         MaterialToolbar topAppBar = findViewById(R.id.topAppBar);
-        topAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                return false;
+        setSupportActionBar(topAppBar);
+        topAppBar.setOnMenuItemClickListener(item -> {
+            int id = item.getItemId();
+            switch (id) {
+                case R.id.settings:
+                    Log.i("UI", "Settings");
+                    return true;
+                case R.id.search:
+                    Log.i("UI", "Search");
+                    return true;
+                default:
+                    Log.i("UI", "Unknow button");
+                    return false;
             }
         });
 
+        handleIntent(getIntent());
+
         Refresh();
+
+
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.i("Search", "Intent called");
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(this, "Searching for " + query, Toast.LENGTH_LONG).show();
+            Log.i("Search", "Search action on " + query);
+            target = query;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.app_bar, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+        return true;
     }
 
     private void Refresh() {
         new Thread(() -> {
             URL url;
             try {
-                url = new URL("https://api.github.com/users/naegon/repos");
+                url = new URL("https://api.github.com/users/" + target + "/repos");
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 //                String basicAuth = "Basic " + Base64.encodeToString(("Naegon:zFqi58Cmvw").getBytes(), Base64.NO_WRAP);
 //                urlConnection.setRequestProperty ("Authorization", basicAuth);
