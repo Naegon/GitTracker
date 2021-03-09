972df6df35da1ef28604869298be734f801058eb
Guillaume Hamel <guillaume.hamel@efrei.net>
2021-03-09 10:56:48 +0100

    Some toolbar theming

--- app/src/main/java/com/test/gittracker/MainActivity.java
+++ app/src/main/java/com/test/gittracker/MainActivity.java
@@ -1,11 +1,15 @@
 package com.test.gittracker;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +26,24 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
 
         tabLayout.setupWithViewPager(viewPager);
+
+        MaterialToolbar topAppBar = findViewById(R.id.topAppBar);
+        setSupportActionBar(topAppBar);
+        topAppBar.setOnMenuItemClickListener(item -> {
+            int id = item.getItemId();
+            switch (id) {
+                case R.id.settings:
+                    Log.i("UI", "Settings");
+                    return true;
+                case R.id.search:
+//                    findViewById(R.id.editTextSearch).setVisibility(View.VISIBLE);
+                    Log.i("UI", "Search");
+                    return true;
+                default:
+                    Log.i("UI", "Unknow button");
+                    return false;
+            }
+        });
 
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 
@@ -46,5 +68,22 @@ public class MainActivity extends AppCompatActivity {
                 // Code goes here
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.app_bar, menu);
+
+        // Associate searchable configuration with the SearchView
+
+//        SearchManager searchManager =
+//                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+//        SearchView searchView =
+//                (SearchView) menu.findItem(R.id.search).getActionView();
+//        searchView.setSearchableInfo(
+//                searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
     }
 }
