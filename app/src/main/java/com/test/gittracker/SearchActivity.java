8887ffc59921aa30b2de3c7df85ad08a7f10d518
Guillaume Hamel <guillaume.hamel@efrei.net>
2021-03-12 22:39:09 +0100

    Better search reaction

--- app/src/main/java/com/test/gittracker/SearchActivity.java
+++ app/src/main/java/com/test/gittracker/SearchActivity.java
@@ -4,33 +4,54 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.lang.ref.WeakReference;
 
 public class SearchActivity extends AppCompatActivity {
-    private String target = "Naegon";
+    private String target;
+    private String searchFor = "user";
+
+    private TextInputEditText textInputEditSearch;
+    private TextInputLayout editTextSearch;
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        TextInputEditText textInputEditSearch = findViewById(R.id.textInputEditSearch);
-        textInputEditSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                target = textInputEditSearch.getEditableText().toString();
-                refresh();
-                return true;
-            }
-        });
+        textInputEditSearch = findViewById(R.id.textInputEditSearch);
+        radioGroup = findViewById(R.id.radioGroup);
+        editTextSearch = findViewById(R.id.editTextSearch);
+
+        textInputEditSearch.setOnEditorActionListener(search);
+        radioGroup.setOnCheckedChangeListener(switchSearchType);
     }
+
+    private final TextView.OnEditorActionListener search = new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            target = textInputEditSearch.getEditableText().toString();
+            refresh();
+            return true;
+        }
+    };
+
+    private final RadioGroup.OnCheckedChangeListener switchSearchType = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            editTextSearch.setHint("Search for a" + ((checkedId == R.id.radio_User)?"n user":" repository"));
+            editTextSearch.setStartIconDrawable((checkedId == R.id.radio_User)?R.drawable.ic_followers:R.drawable.ic_repository);
+        }
+    };
 
     private void refresh() {
         TextView nothingToShow = findViewById(R.id.nothingToShow);
