0bc8d8db4922ecf03096d161ea1bccfeaadc953a
Guillaume Hamel <guillaume.hamel@efrei.net>
2021-03-12 17:56:25 +0100

    Change language

--- app/src/main/java/com/test/gittracker/SettingsActivity.java
+++ app/src/main/java/com/test/gittracker/SettingsActivity.java
@@ -1,13 +1,15 @@
 package com.test.gittracker;
 
-import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.PreferenceFragmentCompat;
-import androidx.preference.PreferenceManager;
+
+import java.util.Locale;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -33,14 +35,27 @@ public class SettingsActivity extends AppCompatActivity {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
 
             findPreference("darkTheme").setOnPreferenceChangeListener((preference, newValue) -> {
-                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(requireContext());
-                boolean darkTheme = sp.getBoolean("darkTheme", false);
+                boolean darkTheme = (Boolean)newValue;
                 if (darkTheme) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                }
-                else {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                 }
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+                requireActivity().recreate();
+
+                return true;
+            });
+
+            findPreference("language").setOnPreferenceChangeListener((preference, newValue) -> {
+                Toast.makeText(getContext(), (CharSequence) newValue, Toast.LENGTH_SHORT).show();
+
+                Locale myLocale = new Locale((String) newValue); // Set Selected Locale
+                Locale.setDefault(myLocale); // set new locale as default
+                Configuration config = new Configuration(); // get Configuration
+                config.setLocale(myLocale); // set config locale as selected locale
+                getActivity().getBaseContext().getResources().updateConfiguration(config, getActivity().getBaseContext().getResources().getDisplayMetrics()); // Update the config
+
                 requireActivity().recreate();
                 return true;
             });
